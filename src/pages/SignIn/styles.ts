0a18f3fa28498d5ba132dbd7f5ead672d856941b
Italo Guglielmini <italo.guglielmini@gmail.com>
2022-09-15 10:24:11 -0300

    fix: ajustes inicial

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
 
-export const Container = styled.View``
+export const Container = styled(LinearGradient).attrs(({theme}) => ({
+    colors: theme.COLORS.
+}))``;
 export const Title = styled.Text``
